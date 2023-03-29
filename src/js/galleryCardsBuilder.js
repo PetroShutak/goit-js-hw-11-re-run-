22bdb35d57607625dfcb11424dbfb31152ac16de
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-03-30 00:17:47 +0300

    Create galleryCardsBuilder.js

---
+++ src/js/galleryCardsBuilder.js
@@ -0,0 +1,36 @@
+export default class GalleryCardsBuilder {
+    buildCards(cardsData) {
+      return cardsData.map(this.buildCard).join(' ');
+    }
+  
+    buildCard(cardData) {
+      return `
+          <li class="gallery__item">
+            <a href="${cardData.largeImageURL}">
+              <div class="photo-card">
+                <img class="photo-card__image" src="${cardData.webformatURL}" alt="${cardData.tags}" loading="lazy" />
+                <div class="info">
+                  <p class="info-item">
+                    <b>Likes</b>
+                    <span>${cardData.likes}</span>
+                  </p>
+                  <p class="info-item">
+                    <b>Views</b>
+                    <span>${cardData.views}</span>
+                  </p>
+                  <p class="info-item">
+                    <b>Comments</b>
+                    <span>${cardData.comments}</span>
+                  </p>
+                  <p class="info-item">
+                    <b>Downloads</b>
+                    <span>${cardData.downloads}</span>
+                  </p>
+                </div>
+              </div> 
+            </a>
+          </li>
+        `;
+    }
+  }
+  
